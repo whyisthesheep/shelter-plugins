70f23818d41aac5c9e39e5dd31ac9ae666c6316d
whyisthesheep <whyisthesheep@users.noreply.github.com>
2024-04-11 14:43:52 +0000

    deploy: 5fce2a7ddcf855a4e90b40468d5050ce01855f0b

--- anselcord/plugin.js
+++ anselcord/plugin.js
@@ -27,37 +27,24 @@
       log
     }
   } = shelter;
-  var menuimage = document.querySelector("#app-mount > div.appAsidePanelWrapper__5e6e2 > div.notAppAsidePanel__95814 > div.app_b1f720 > div > div.layers__1c917.layers_a23c37 > div > div > nav > ul > div.scroller_de945b.none_ff9f86.scrollerBase__65223 > div.tutorialContainer_b06ae6 > div > div.listItemWrapper__3d465.selected__501f5 > div > svg > foreignObject > div > div > img");
-  var menusvg = document.querySelector("#app-mount > div.appAsidePanelWrapper__5e6e2 > div.notAppAsidePanel__95814 > div.app_b1f720 > div > div.layers__1c917.layers_a23c37 > div > div > nav > ul > div.scroller_de945b.none_ff9f86.scrollerBase__65223 > div.tutorialContainer_b06ae6 > div > div.listItemWrapper__3d465 > div > svg > foreignObject > div > div > svg");
   function onLoad() {
-    if (menuimage) {
-      log("Menu img");
-      const udimagei = document.createElement("img");
-      udimagei.src = "https://0x0.st/Xikk.png";
-      udimagei.style.width = "48px";
-      udimagei.style.height = "48px";
-      udimagei.style.objectFit = "contain";
-      udimagei.style.display = "block";
-      udimagei.style.margin = "auto";
-      udimagei.style.position = "absolute";
-      udimagei.style.top = "50%";
-      udimagei.style.left = "50%";
-      udimagei.style.transform = "translate(-50%, -50%)";
-      menuimage.replaceWith(udimagei);
-    } else if (menusvg) {
-      log("Menu svg");
-      const udimages = document.createElement("img");
-      udimages.src = "https://0x0.st/Xikk.png";
-      udimages.style.width = "48px";
-      udimages.style.height = "48px";
-      udimages.style.objectFit = "contain";
-      udimages.style.display = "block";
-      udimages.style.margin = "auto";
-      udimages.style.position = "absolute";
-      udimages.style.top = "50%";
-      udimages.style.left = "50%";
-      udimages.style.transform = "translate(-50%, -50%)";
-      menusvg.replaceWith(udimages);
+    const svgElement = document.querySelector("#app-mount > div.appAsidePanelWrapper__5e6e2 > div.notAppAsidePanel__95814 > div.app_b1f720 > div > div.layers__1c917.layers_a23c37 > div > div > nav > ul > div.scroller_de945b.none_ff9f86.scrollerBase__65223 > div.tutorialContainer_b06ae6 > div > div.listItemWrapper__3d465.selected__501f5 > div > svg > foreignObject > div > div > svg");
+    if (svgElement) {
+      log("SVG element found!");
+      const imgElement = document.createElement("img");
+      imgElement.src = "https://0x0.st/Xikk.png";
+      imgElement.style.width = "48px";
+      imgElement.style.height = "48px";
+      imgElement.style.objectFit = "contain";
+      imgElement.style.display = "block";
+      imgElement.style.margin = "auto";
+      imgElement.style.position = "absolute";
+      imgElement.style.top = "50%";
+      imgElement.style.left = "50%";
+      imgElement.style.transform = "translate(-50%, -50%)";
+      svgElement.parentNode.replaceChild(imgElement, svgElement);
+    } else {
+      log("Discord logo not found. Checking again in 1 second...");
     }
   }
   return __toCommonJS(anselcord_exports);
