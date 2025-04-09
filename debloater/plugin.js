240d9d31fc9ac55a6be56b59f88f984c336f63c8
whyisthesheep <whyisthesheep@users.noreply.github.com>
2025-04-09 10:46:11 +0000

    deploy: a54b2b6123b1103f463db7598cc4a4c0d8e37325

--- debloater/plugin.js
+++ debloater/plugin.js
@@ -1,29 +1,10 @@
-(() => {
-  var __defProp = Object.defineProperty;
-  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-  var __getOwnPropNames = Object.getOwnPropertyNames;
-  var __hasOwnProp = Object.prototype.hasOwnProperty;
-  var __export = (target, all) => {
-    for (var name in all)
-      __defProp(target, name, { get: all[name], enumerable: true });
-  };
-  var __copyProps = (to, from, except, desc) => {
-    if (from && typeof from === "object" || typeof from === "function") {
-      for (let key of __getOwnPropNames(from))
-        if (!__hasOwnProp.call(to, key) && key !== except)
-          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-    }
-    return to;
-  };
-  var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+(function(exports) {
 
-  // plugins/debloater/index.jsx
-  var debloater_exports = {};
-  __export(debloater_exports, {
-    onLoad: () => onLoad
-  });
-  function onLoad() {
-    const customCSS = `
+"use strict";
+
+//#region plugins/debloater/index.jsx
+function onLoad() {
+	const customCSS = `
         button[aria-label="Open sticker picker"],
         button[aria-label="Open GIF picker"],
         button[aria-label="Send a gift"],
@@ -39,10 +20,13 @@
             display: none !important;
         }
     `;
-    const styleElement = document.createElement("style");
-    styleElement.textContent = customCSS;
-    document.head.appendChild(styleElement);
-    console.log("Debloated CSS applied successfully!");
-  }
-  return __toCommonJS(debloater_exports);
-})();
+	const styleElement = document.createElement("style");
+	styleElement.textContent = customCSS;
+	document.head.appendChild(styleElement);
+	console.log("Debloated CSS applied successfully!");
+}
+
+//#endregion
+exports.onLoad = onLoad
+return exports;
+})({});
